c68424adfd9cc58fb88753eb0d9af37e84410d98
Jamie Bort <jamiebort@gmail.com>
2023-03-27 18:07:37 -0600

    Prepare to push to gh-pages.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
 
+let status1;
+let status2;
+
+const A_SECRET = process.env.MY_FIRST_SECRET;
+const A_DIFFERENT_SECRET = process.env.github_secret;
+
+if(A_SECRET) status1 = true;
+else status1 = false;
+
+if(A_DIFFERENT_SECRET) status2 = true;
+else status2 = false;
+
+
 function App() {
+
+  // const renderSwitch =(param)=> {
+  //       switch(param) {
+  //         case A_SECRET:
+  //           return 'MY_FIRST_SECRET';
+  //         case A_DIFFERENT_SECRET:
+  //           return 'github_secret';
+  //         default:
+  //           return 'there is no secret';
+  //       }
+  //     }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>"My FIRST SECRET" <b>{status1 ? A_SECRET : "does not exist"}</b>.</p>
+        <p>"my first secret" <b>{status2 ? A_DIFFERENT_SECRET : "does not exist"}</b>.</p>
+        {/* <p>{renderSwitch("foo")}</p> */}
+        
       </header>
     </div>
   );
